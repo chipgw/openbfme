0f31168f5d4a66a4456f13fd0bdd92868304ff66
chipgw <gw.chip.gw@gmail.com>
2014-12-13 10:47:31 -0600

    Added GLM dependency.
    Added typedefs for vec3 and vec4 to types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -4,14 +4,20 @@
 #include "build_platform.hpp"
 #include <string>
 #include <cstdint>
+#include <glm/vec4.hpp>
 
 namespace OpenBFME {
 
 typedef int integer;
 typedef float decimal;
+
 typedef char character;
+
 typedef const character* cstring;
 typedef std::basic_string<character> string;
+
+typedef glm::vec3 vec3;
+typedef glm::vec4 vec4;
 
 }
 
