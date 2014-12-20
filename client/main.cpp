3003893119482d33db39f2cbda663503591a519b
chipgw <gw.chip.gw@gmail.com>
2014-12-20 12:59:21 -0600

    Added stub main functions to client and server. Some CMake cleanup.

--- client/main.cpp
+++ client/main.cpp
@@ -1,1 +1,11 @@
- 
+#include "log.hpp"
+
+using namespace OpenBFME;
+
+int main() {
+    Log::init("openbfme_client.log");
+
+    Log::error("OpenBFME client not yet implemented!");
+
+    return 0;
+}
--- server/main.cpp
+++ server/main.cpp
@@ -1,1 +1,11 @@
- 
+#include "log.hpp"
+
+using namespace OpenBFME;
+
+int main() {
+    Log::init("openbfme_server.log");
+
+    Log::error("OpenBFME server not yet implemented!");
+
+    return 0;
+}
