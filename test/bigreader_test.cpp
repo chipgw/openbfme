e0a2388830f70e7ef8a4063cfd31935c01c5a6a3
chipgw <gw.chip.gw@gmail.com>
2015-04-01 20:02:02 -0500

    bigreader_test now keeps track of failed/total ratio.

--- test/bigreader_test.cpp
+++ test/bigreader_test.cpp
@@ -10,7 +10,7 @@ bool testFile(const BigEntry* file){
         return false;
     }
     for(int i = 0; !file->eof(); ++i){
-        Log::info("Line #%04d: \"%s\"", i + 1, file->getLine(false));
+        Log::debug("Line #%04d: \"%s\"", i + 1, file->getLine(false));
     }
     return true;
 }
@@ -20,27 +20,43 @@ int main(int argc, const char* argv[]){
 
     app.parseArguments();
 
+
+    int failed = 0;
+
+    const static std::vector<std::pair<string, string>> tests {
+        {"folder/inifile.ini",  ""},
+        {"inifile.ini",         "folder\\inifile.ini"},
+        {"inifile.ini",         "folder/"},
+        {"..\\NewFile.inc",     "folder\\inifile.ini"},
+        {"..\\NewFile.inc",     "folder/inifile.ini"},
+    };
+
     BigFilesystem big;
 
     big.mount("test.big", true);
 
-    testFile(big.openFile("folder/inifile.ini"));
-    testFile(big.openFile("inifile.ini", "folder\\inifile.ini"));
-    testFile(big.openFile("inifile.ini", "folder/"));
-    testFile(big.openFile("..\\NewFile.inc", "folder\\inifile.ini"));
-    testFile(big.openFile("..\\NewFile.inc", "folder/inifile.ini"));
+    for(integer i = 0; i < tests.size(); ++i){
+        Log::info("Test #%d:", i);
+        if(!testFile(big.openFile(tests[i].first, tests[i].second)))
+            failed++;
+    }
 
     big.unmount("test.big");
 
     big.mount("test", true);
 
-    testFile(big.openFile("folder/inifile.ini"));
-    testFile(big.openFile("inifile.ini", "folder\\inifile.ini"));
-    testFile(big.openFile("inifile.ini", "folder/"));
-    testFile(big.openFile("..\\NewFile.inc", "folder\\inifile.ini"));
-    testFile(big.openFile("..\\NewFile.inc", "folder/inifile.ini"));
+    for(integer i = 0; i < tests.size(); ++i){
+        Log::info("Test #%d:", i + tests.size());
+        if(!testFile(big.openFile(tests[i].first, tests[i].second)))
+            failed++;
+    }
 
     big.unmount("test");
+
+    if(failed == 0)
+        Log::info("All %i tests successful!", integer(tests.size()) * 2);
+    else
+        Log::error("%i/%i test(s) failed!", failed, integer(tests.size()) * 2);
 
     return 0;
 }
