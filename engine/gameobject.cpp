c586bf355df0254741753fef559b8c94c1dec096
chipgw <gw.chip.gw@gmail.com>
2015-07-08 08:45:12 -0500

    createObject() was not passing objectName to GameObject constructor. Comments and stuff.

--- engine/gameobject.cpp
+++ engine/gameobject.cpp
@@ -10,6 +10,8 @@ GameObject::GameObject(GameWorld& owner, const IniObject& objTemplate, const str
 
 void GameObject::tick(decimal delta) {
     Log::debug("Object ticked. Object Name: \"%s\" Delta: %f", name, delta);
+
+    /* TODO - There is a heck of a lot of stuff that belongs here... */
 }
 
 }
--- engine/gameworld.cpp
+++ engine/gameworld.cpp
@@ -19,13 +19,21 @@ void GameWorld::tick(decimal delta) {
 }
 
 std::weak_ptr<GameObject> GameWorld::createObject(const string& templateName, const string& objectName) {
+    /* Find an Object tempolate with a matching name. */
     auto objTemplate = std::find_if(iniRoot.subObjects.cbegin(), iniRoot.subObjects.cend(), [&](const std::pair<string, IniObject> def) {
         return def.first == "Object" && !def.second.args.empty() && def.second.args[0] == templateName;
     });
+
+    /* If a template was found, create the Object. */
     if (objTemplate != iniRoot.subObjects.cend()) {
-        objects.emplace_back(new GameObject(*this, objTemplate->second));
+        objects.emplace_back(new GameObject(*this, objTemplate->second, objectName));
+
+        Log::debug("Object created with template \"%s\".", templateName);
+
         return objects.back();
     }
+
+    /* If no template was found return an empty weak_ptr. */
     return std::weak_ptr<GameObject>();
 }
 
