44f993503da3a927526c5a10012c73e239b3b2c4
chipgw <gw.chip.gw@gmail.com>
2015-10-20 10:06:51 -0700

    Simplify variable types in IniType definition files.

--- iniparser/initype.cpp
+++ iniparser/initype.cpp
@@ -13,14 +13,14 @@ namespace {
 IniType makeDefinitionType(integer recursion) {
     IniType type;
 
-    type.variableTypes.emplace("BoolVariable",       IniVariable::String);
-    type.variableTypes.emplace("IntegerVariable",    IniVariable::String);
-    type.variableTypes.emplace("DecimalVariable",    IniVariable::String);
-    type.variableTypes.emplace("PercentVariable",    IniVariable::String);
-    type.variableTypes.emplace("StringVariable",     IniVariable::String);
-    type.variableTypes.emplace("ColorVariable",      IniVariable::String);
-    type.variableTypes.emplace("VectorVariable",     IniVariable::String);
-    type.variableTypes.emplace("LineVariable",       IniVariable::String);
+    type.variableTypes.emplace("Bool",      IniVariable::String);
+    type.variableTypes.emplace("Integer",   IniVariable::String);
+    type.variableTypes.emplace("Decimal",   IniVariable::String);
+    type.variableTypes.emplace("Percent",   IniVariable::String);
+    type.variableTypes.emplace("String",    IniVariable::String);
+    type.variableTypes.emplace("Color",     IniVariable::String);
+    type.variableTypes.emplace("Vector",    IniVariable::String);
+    type.variableTypes.emplace("Line",      IniVariable::String);
 
     if(recursion > 0)
         type.subTypes.emplace("SubType", makeDefinitionType(recursion - 1));
@@ -55,21 +55,21 @@ bool IniType::loadFromIni(const string& filename) {
 
 bool IniType::loadFromIni(const IniObject& object) {
     for(auto& var : object.variables){
-        if(var.first == "BoolVariable")
+        if(var.first == "Bool")
             variableTypes[var.second.s] = IniVariable::Bool;
-        else if(var.first == "IntegerVariable")
+        else if(var.first == "Integer")
             variableTypes[var.second.s] = IniVariable::Integer;
-        else if(var.first == "DecimalVariable")
+        else if(var.first == "Decimal")
             variableTypes[var.second.s] = IniVariable::Decimal;
-        else if(var.first == "PercentVariable")
+        else if(var.first == "Percent")
             variableTypes[var.second.s] = IniVariable::Percent;
-        else if(var.first == "StringVariable")
+        else if(var.first == "String")
             variableTypes[var.second.s] = IniVariable::String;
-        else if(var.first == "ColorVariable")
+        else if(var.first == "Color")
             variableTypes[var.second.s] = IniVariable::Color;
-        else if(var.first == "VectorVariable")
+        else if(var.first == "Vector")
             variableTypes[var.second.s] = IniVariable::Vector;
-        else if(var.first == "LineVariable")
+        else if(var.first == "Line")
             variableTypes[var.second.s] = IniVariable::Line;
 
         Log::debug("Variable type \"%s\" for variable \"%s\".", var.first, var.second.s);
